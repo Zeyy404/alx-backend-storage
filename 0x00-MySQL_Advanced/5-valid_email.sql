87240cb7f1de55aa888e5c1df926c999444fa266
Zainab Salih <Zainab.Salih@outlook.com>
2024-08-22 22:47:23 +0400

    Task 5{modified}

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -4,7 +4,7 @@ CREATE TRIGGER reset_valid_email
 AFTER UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF OLD.email != NEW.email THEN
+    IF NEW.email != OLD.email THEN
         SET NEW.valid_email = 0;
     END IF;
 END;
