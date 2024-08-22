f11eeb0d44f7dc8a758403e54ff41e421dce0d77
Zainab Salih <Zainab.Salih@outlook.com>
2024-08-22 18:12:00 +0000

    Task 2{modified}

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,10 +1,10 @@
 -- Rank countries by the number of fans
 SELECT
     origin,
-    SUM(nb_fans) AS total_fans
+    SUM(fans) AS nb_fans
 FROM
     metal_bands
 GROUP BY
     origin
 ORDER BY
-    total_fans DESC;
+    nb_fans DESC;
