b573c5ff02ea95ba784522de03fc752c7f981b58
Zainab Salih <Zainab.Salih@outlook.com>
2024-08-22 22:27:49 +0400

    Task 3{modified} & Task 4

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -4,11 +4,9 @@ DELIMITER //
 CREATE TRIGGER decrease_item_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
-BEGIN
     UPDATE items
     SET quantity = quantity - NEW.number
     WHERE name = NEW.name;
-END;
 //
 
 DELIMITER ;
