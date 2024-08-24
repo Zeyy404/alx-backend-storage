034bfc5f8b0bb39f7477644bec46da37b58d836e
Zainab Salih <Zainab.Salih@outlook.com>
2024-08-24 17:14:21 +0400

    Task 7

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -6,11 +6,11 @@ CREATE PROCEDURE AddBonus(
   IN score INT
 )
 BEGIN
-  IF NOT EXISTS(SELECT name FROM projects WHERE name=project_name) THEN
+  IF NOT EXISTS(SELECT name FROM projects WHERE name = project_name) THEN
 		INSERT INTO projects (name) VALUES (project_name);
 	END IF;
 	INSERT INTO corrections (user_id, project_id, score)
-	VALUES (user_id, (SELECT id from projects WHERE name=project_name), score);
+	VALUES (user_id, (SELECT id from projects WHERE name = project_name), score);
 END;
 //
 DELIMITER ;
--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,12 +2,12 @@
 -- that computes and store the average score for a student. Note: An average score can be a decimal.
 DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(
-  IN user_id INT
+  IN p_user_id INT
 )
 BEGIN
   DECLARE avg_score DECIMAL(10, 2);
-  SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
-  UPDATE users SET average_score = avg_score WHERE id = user_id;
+  SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = p_user_id;
+  UPDATE users SET average_score = avg_score WHERE id = p_user_id;
 END;
 //
 DELIMITER ;
