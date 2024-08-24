68b2f13f3c31d766242d8c08a65eda8d48938801
Zainab Salih <Zainab.Salih@outlook.com>
2024-08-24 16:51:28 +0400

    Task 6

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -6,15 +6,11 @@ CREATE PROCEDURE AddBonus(
   IN score INT
 )
 BEGIN
-  DECLARE project_id INT;
-  SELECT id INTO project_id FROM projects WHERE name = project_name;
-  IF project_id IS NULL THEN
-    INSERT INTO projects (name)
-    VALUES (project_name);
-    SELECT LAST_INSERT_ID() INTO project_id
-  END IF;
-  INSERT INTO corrections (user_id, project_id, score)
-  VALUES (user_id, project_id, score)
+  IF NOT EXISTS(SELECT name FROM projects WHERE name=project_name) THEN
+		INSERT INTO projects (name) VALUES (project_name);
+	END IF;
+	INSERT INTO corrections (user_id, project_id, score)
+	VALUES (user_id, (SELECT id from projects WHERE name=project_name), score);
 END;
 //
 DELIMITER ;
